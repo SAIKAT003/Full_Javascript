62f6adda39e720c3b5b5214feb41c929bcf9a169
SAIKAT SAHA <115502880+SAIKAT003@users.noreply.github.com>
2024-04-06 16:00:13 +0000

    convertions

---
+++ 01_basics/03_convertionOperations.js
@@ -0,0 +1,14 @@
+let score = "33abs"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let number = Number(score);
+
+// console.log(typeof number);
+// console.log(number);
+
+let state = "";
+
+let boolState = Boolean(state);
+console.log(boolState);
