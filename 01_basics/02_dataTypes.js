8bb03496e72fd3ef9bd884daf1eb0ab9ca13032d
SAIKAT SAHA <115502880+SAIKAT003@users.noreply.github.com>
2024-04-06 15:59:40 +0000

    data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+let name = "saikat" //String
+let age = 21 //number
+//bigint
+//boolean => T/F
+//number
+//null => standalone value
+//undefined
+//symble => Unique
+
+//object
+
+console.log(typeof null);
