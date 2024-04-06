f45f69355e5fc8f9c8523d2a47a4074d5a95cb30
SAIKAT SAHA <115502880+SAIKAT003@users.noreply.github.com>
2024-04-06 15:59:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId  = 1455442  //constant
+let accounntEmail = "saikatsaha8659@gmail.com" //variable
+var accountPassword = "12345" //variavble(not recomended to use var , cause-scope)
+let accountState //undifined
+accountCity = "Kolkata"
+
+console.log(accountId);
+console.table([accountId,accounntEmail,accountPassword,accountCity])
+
+accounntEmail ="sas@sda"
+console.table([accountId,accounntEmail,accountPassword,accountCity,accountState])
